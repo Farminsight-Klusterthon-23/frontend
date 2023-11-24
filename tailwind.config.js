572188726c231deae93b7a99fcf005abc2fd775a
exploitenomah <iksploiting@gmail.com>
2023-11-24 08:05:54 +0100

    define tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundImage: {},
+      boxShadow: {
+        "dashboard-sidenav": "0px 17px 40px 4px rgba(112, 144, 176, 0.11)",
+        "dropdown-menu": "0px 4px 25px 0px rgba(0, 0, 0, 0.10)"
+      },
+      colors: {
+        primary: {
+          main: "#034732",
+          medium: "#1B2559",
+          light: "#4C4C4C",
+          "contrast-text": "#F7F7F7",
+        },
+        secondary: {
+          dark: "#718096",
+          light: "#635B5B",
+          medium: "#3C3C3C",
+          "contrast-text": "#F2F7FF"
+        },
+        accent:{
+          primary: "#FF6E6E",
+        }
       },
     },
   },
