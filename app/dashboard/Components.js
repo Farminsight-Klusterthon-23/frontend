6de5065cbb9b612f0b61fe12066bfe26970c0d87
exploitenomah <iksploiting@gmail.com>
2023-11-24 15:22:15 +0100

    create display toggler for dashboard

--- app/dashboard/Components.js
+++ app/dashboard/Components.js
@@ -7,6 +7,7 @@ import {
   AcademicArticlesSvg,
   ProduceInsightsSvg,
   SmallArrowSvg,
+  FileSvg,
 } from "./Svgs"
 import NextLink from "next/link"
 import { useState, useRef, useCallback } from "react"
@@ -183,20 +184,45 @@ export function ModeTagBtn({ currentMode, mode, Icon, toggleCurrentMode }) {
 }
 
 export function ModeTags({ currentMode, toggleCurrentMode }) {
+  const buttons = [
+    { icon: ChatSvg, name: "chat" },
+    { icon: OfflineSvg, name: "offline" },
+  ]
   return (
     <div className="flex items-center justify-center text-[1.125rem] font-700 font-pjs">
-      <ModeTagBtn
-        currentMode={currentMode}
-        toggleCurrentMode={toggleCurrentMode}
-        Icon={ChatSvg}
-        mode="chat"
-      />
-      <ModeTagBtn
-        currentMode={currentMode}
-        toggleCurrentMode={toggleCurrentMode}
-        Icon={OfflineSvg}
-        mode="offline"
-      />
+      {buttons.map((button) => (
+        <ModeTagBtn
+          key={button.name}
+          currentMode={currentMode}
+          toggleCurrentMode={toggleCurrentMode}
+          Icon={button.icon}
+          mode={button.name}
+        />
+      ))}
     </div>
+  )
+}
+
+export function OfflineDisplay({ hasOfflineItems }) {
+  if (!hasOfflineItems) return <NoOfflineItemsDisplay />
+  return <section></section>
+}
+
+export function NoOfflineItemsDisplay() {
+  return (
+    <section className="text-center mx-auto">
+      <span className="block mb-[16px] mx-auto w-fit">
+        <FileSvg />
+      </span>
+      <h1 className="font-[600] text-[1.125rem]">
+        You dont have any offline content saved yet!
+      </h1>
+      <p className="font-montserrat font-[400] text-[1.125rem] mb-[8px]">
+        Searches you save for later can be found here even without a network.
+      </p>
+      <button className="bg-primary-main mx-auto text-white text-base py-[16px] px-[12px] rounded-[4px]">
+        Start asking questions
+      </button>
+    </section>
   )
 }
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,21 +1,35 @@
 "use client"
 import AuthenticatedScreensLayout from "../_components/AppLayout"
-import { ModeTags, ChatBoxContainer } from "./Components"
+import { ModeTags, ChatBoxContainer, OfflineDisplay } from "./Components"
 import { useState } from "react"
 
 export default function DashBoard() {
   const [currentModeView, setCurrentModeView] = useState("chat")
   return (
-    <AuthenticatedScreensLayout pageTitle="Home">
-      <div className="flex flex-col items-center gap-y-[28px] w-[95dvw] sm:w-[95%] max-w-[915px] mx-auto">
-        <ModeTags
-          currentMode={currentModeView}
-          toggleCurrentMode={(view) => setCurrentModeView(view)}
-        />
-        <h1 className="text-center text-[2rem] md:text-[3rem] text-primary-main">
-          Welcome to FarmInsight
-        </h1>
-        <ChatBoxContainer />
+    <AuthenticatedScreensLayout
+      pageTitle="Home"
+      centerChildrenVertically={false}
+    >
+      <div className=" min-h-[calc(100dvh-150px)] flex flex-col items-center gap-y-[28px] w-[95dvw] sm:w-[95%] max-w-[915px] mx-auto">
+        <div className="sticky top-0">
+          <ModeTags
+            currentMode={currentModeView}
+            toggleCurrentMode={(view) => setCurrentModeView(view)}
+          />
+        </div>
+
+        {currentModeView === "chat" ? (
+          <>
+            <h1 className="text-center text-[2rem] md:text-[3rem] text-primary-main">
+              Welcome to FarmInsight
+            </h1>
+            <ChatBoxContainer />
+          </>
+        ) : (
+          <div className="mt-[10%]">
+            <OfflineDisplay />
+          </div>
+        )}
       </div>
     </AuthenticatedScreensLayout>
   )
