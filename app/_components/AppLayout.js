e6a199747638cd1aa7882560f0461325949fc444
exploitenomah <iksploiting@gmail.com>
2023-11-25 17:23:40 +0100

    update layout styles to prevent overflow

--- app/_components/AppLayout.js
+++ app/_components/AppLayout.js
@@ -7,8 +7,8 @@ export default function AuthenticatedScreensLayout({ children, pageTitle }) {
   const [showSidebar, setShowSidebar] = useState(false)
 
   return (
-    <div className="flex bg-primary-contrast-text">
-      <div>
+    <div className="flex bg-primary-contrast-text h-screen overflow-hidden">
+      <div className="h-screen overflow-y-auto min-w-[max-content]">
         <Sidebar show={showSidebar} hideSidebar={() => setShowSidebar(false)} />
       </div>
       <div
@@ -21,7 +21,7 @@ export default function AuthenticatedScreensLayout({ children, pageTitle }) {
         <div
           className={`${
             showSidebar ? "overflow-hidden" : "overflow-auto"
-          } mx-auto flex justify-center items-center min-h-[calc(100dvh-94px)]`}
+          } mx-auto flex justify-center items-center h-[calc(100dvh-94px)]`}
         >
           <main className="w-full grow">{children}</main>
         </div>
